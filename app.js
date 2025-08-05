ea7cf74ba086576f3c260d108e2e3eb6a3ad0a8a
alimajd001-dot <alimajd001@gmail.com>
2025-08-05 10:47:41 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function startBot() {
+  document.getElementById("output").innerText = "🤖 ربات ترید فعال شد!";
+}
+function showReport() {
+  document.getElementById("output").innerText = "📈 گزارش: سود کل ۲.۳٪ در ۲۴ ساعت گذشته.";
+}
